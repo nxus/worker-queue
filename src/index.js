dca7beee2bc2a521a7c3a2a5e66de7324518c3d0
Luke Opperman <luke@opperman.net>
2016-02-17 12:29:02 -0500

    Stub implementation

--- src/index.js
+++ src/index.js
@@ -6,11 +6,59 @@
 
 'use strict';
 
-/**
- * The Base Nxus Module
- */
-export default class Module {
-  constructor(app) {
+const _defaultConfig = {
+  redis_url: process.env.REDIS_URL || 'redis://localhost'
+}
 
+/**
+ * Worker Queue module for background tasks
+ */
+export default class WorkerQueue {
+  constructor(app) {
+    this.app = app
+    this.config = Object.assign(_defaultConfig, app.config['worker-queue'])
+
+    app.get('worker-queue').use(this)
+      .gather('worker')
+      .respond('task')
+      
+    app.onceAfter('load', this._connect.bind(this))
+    app.once('stop', this._disconnect.bind(this))
+  }
+
+  // Handlers
+
+  /**
+   * Provide a task handler
+   * @param {string} taskName Name of the task (channel) to listen for
+   * @param {function} handler Handler for processing task requests
+   * @example app.get('worker-queue').worker('backgroundJob', (msg) -> {})
+   */
+  
+  worker (taskName, handler) {
+    this.app.log.debug('Registering task worker for', taskName)
+    
+  }
+
+  /**
+   * Request handling of a background task
+   * @param {string} taskName Name of the task (channel) to publish to
+   * @param {object} message Options for the task worker
+   * @example app.get('worker-queue').task('backgroundJob', {hi: 'world'})
+   */
+  task (taskName, message) {
+    this.app.log.debug('Task requested', taskName)
+    
+  }
+  
+
+  // Internal
+
+  _connect () {
+    
+  }
+
+  _disconnect () {
+    
   }
 } 
--- test/index.js
+++ test/index.js
@@ -6,24 +6,46 @@
 
 'use strict';
 
-import Module from '../src/'
+import WorkerQueue from '../src/'
 
 import TestApp from '@nxus/core/lib/test/support/TestApp';
 
-describe("Module", () => {
+describe("Worker Queue", () => {
   var module, app;
  
-  beforeEach(() => {
+  beforeEach(function() {
+    this.timeout(3000)
     app = new TestApp();
-    module = new Module(app);
   });
   
   describe("Load", () => {
-    it("should not be null", () => Module.should.not.be.null)
+    it("should not be null", () => WorkerQueue.should.not.be.null)
 
     it("should be instantiated", () => {
-      module = new Module(app);
+      module = new WorkerQueue(app);
       module.should.not.be.null;
     });
+  });
+  describe("Init", () => {
+    beforeEach(() => {
+      module = new WorkerQueue(app);
+    });
+
+    it("should register for app lifecycle", () => {
+      app.once.called.should.be.true;
+      app.onceAfter.calledWith('load').should.be.true;
+      app.once.calledWith('stop').should.be.true;
+    });
+    it("should register a gather for workers", () => {
+      return app.emit('load').then(() => {
+        app.get.calledWith('worker-queue').should.be.true;
+        app.get().gather.calledWith('worker').should.be.true;
+      });
+    })
+    it("should register a handler for task", () => {
+      return app.emit('load').then(() => {
+        app.get().respond.calledWith('task').should.be.true;
+      });
+    })
   });
 })
